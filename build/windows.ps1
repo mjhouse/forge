1aa88d665f59b95882896d4fee5ad8097cd86cf7
Michael House <mjhouse@protonmail.com>
2020-03-30 14:56:01 -0500

    Modified main.yml

--- build/windows.ps1
+++ build/windows.ps1
@@ -1,4 +1,3 @@
-#cmd.exe /c "call `"C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Auxiliary/Build/vcvars64.bat`" && set > %temp%\vcvars.txt"
 cmd.exe /c "call `"C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxiliary/Build/vcvars64.bat`" && set > %temp%\vcvars.txt"
 
 Get-Content "$env:temp\vcvars.txt" | Foreach-Object {
@@ -7,7 +6,14 @@ Get-Content "$env:temp\vcvars.txt" | Foreach-Object {
     }
 }
 
+# create move to output directory
 mkdir bin\\Release
 cd bin/Release
+
+Write-Output "\n"
+
+# generate build files
 cmake ../..
+
+# build libraries and executables
 cmake --build . --config Release
